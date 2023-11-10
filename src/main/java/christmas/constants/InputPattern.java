a406912e05ce84f9131eb3d417b54254f3a79ce3
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 01:18:19 +0900

    feat(InputPattern): 주문 패턴 판별 기능 구현

--- src/main/java/christmas/constants/InputPattern.java
+++ src/main/java/christmas/constants/InputPattern.java
@@ -3,7 +3,8 @@ package christmas.constants;
 import java.util.regex.Pattern;
 
 public enum InputPattern {
-    NUMERIC_PATTERN(Pattern.compile("^\\d*$"));
+    NUMERIC_PATTERN(Pattern.compile("^\\d*$")),
+    ORDER_PATTERN(Pattern.compile("([가-힣a-zA-Z]+)-([1-9])"));
 
     private final Pattern pattern;
 
@@ -14,6 +15,11 @@ public enum InputPattern {
     public static boolean isNotNumeric(final String visitDate) {
         Pattern pattern = NUMERIC_PATTERN.pattern;
         return !pattern.matcher(visitDate).matches();
+    }
+
+    public static boolean isNotOrderPattern(final String order) {
+        Pattern pattern = ORDER_PATTERN.pattern;
+        return !pattern.matcher(order).matches();
     }
 }
 
