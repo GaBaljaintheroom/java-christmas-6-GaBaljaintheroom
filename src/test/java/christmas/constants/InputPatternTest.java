26be24d997f811392b9afadac0f112bd4b62f4a1
GaBaljaintheroom <junsu1222@naver.com>
2023-11-10 17:22:21 +0900

    test(InputPatternTest): 입력 패턴 상수 숫자 패턴 테스트

---
+++ src/test/java/christmas/constants/InputPatternTest.java
@@ -0,0 +1,31 @@
+package christmas.constants;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("입력 패턴 상수에 대해 ")
+class InputPatternTest {
+
+    @Test
+    @DisplayName("숫자 패턴이 아니라면 true를 반환한다.")
+    void When_IsNotNumeric_Then_ReturnTrue() {
+        //when
+        boolean actual = InputPattern.isNotNumeric("박준수");
+
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("숫자 패턴이 맞으면 false를 반환한다.")
+    void When_IsNumeric_Then_ReturnFalse() {
+        //when
+        boolean actual = InputPattern.isNotNumeric("123");
+
+        //then
+        assertFalse(actual);
+    }
+}
