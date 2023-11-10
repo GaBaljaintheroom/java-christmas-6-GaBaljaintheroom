de4494076f2900b38a18732675678dfa65d46915
GaBaljaintheroom <junsu1222@naver.com>
2023-11-10 16:11:03 +0900

    feat(VisitDate): 방문 날짜 1~31일 인지 확인 검증 기능 구현

---
+++ src/main/java/christmas/domain/VisitDate.java
@@ -0,0 +1,27 @@
+package christmas.domain;
+
+import christmas.exception.InvalidDateException;
+
+import java.time.LocalDate;
+
+public class VisitDate {
+
+    private static final int CURRENT_YEAR = 2023;
+    private static final int CURRENT_MONTH = 12;
+
+    private static final int START_DAY = 1;
+    private static final int LAST_DAY = 31;
+
+    private final LocalDate currentDate;
+
+    public VisitDate(final int currentDate) {
+        validateDatePeriod(currentDate);
+        this.currentDate = LocalDate.of(CURRENT_YEAR, CURRENT_MONTH, currentDate);
+    }
+
+    private void validateDatePeriod(final int currentDate) {
+        if (currentDate < START_DAY || currentDate > LAST_DAY) {
+            throw new InvalidDateException();
+        }
+    }
+}
