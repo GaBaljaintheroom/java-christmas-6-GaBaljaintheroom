7a1d17a875bb97eccbc5e74ac870785322096617
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 02:06:34 +0900

    feat(ErrorMessage): 유효하지 않은 주문 에러 메시지 작성

--- src/main/java/christmas/constants/ErrorMessage.java
+++ src/main/java/christmas/constants/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     ERROR_TAG("[ERROR] "),
     INVALID_DATE_ERROR("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    DOMAIN_NULL_ERROR("객체가 초기화되어 있지 않습니다.");
+    DOMAIN_NULL_ERROR("객체가 초기화되어 있지 않습니다."),
+    INVALID_ORDER_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
