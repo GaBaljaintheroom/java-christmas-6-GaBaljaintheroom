70fb596c31c96528fd815ec87b522b65f172eb64
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 02:04:06 +0900

    feat(InputValidator): 주문 패턴 검증 기능 구현

--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -2,12 +2,27 @@ package christmas.validator;
 
 import christmas.constants.InputPattern;
 import christmas.exception.InvalidDateException;
+import christmas.exception.InvalidOrderException;
+
+import java.util.Arrays;
 
 public class InputValidator {
+
+    private static final String DIVISION = ",";
 
     public void validateNumericDate(final String visitDate) {
         if (InputPattern.isNotNumeric(visitDate)) {
             throw new InvalidDateException();
         }
     }
+
+    public void validateOrderPattern(final String order) {
+        Arrays.stream(order.split(DIVISION))
+                .filter(InputPattern::isNotOrderPattern)
+                .findAny()
+                .ifPresent(o -> {
+                    throw new InvalidOrderException();
+                });
+    }
+
 }
