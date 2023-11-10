c8f4ec2a4385a16aaa8112d25d0e824509d3e7f8
GaBaljaintheroom <junsu1222@naver.com>
2023-11-10 15:50:16 +0900

    feat(InputPattern): 숫자 입력 패턴 상수인지 판별 기능 구현

---
+++ src/main/java/christmas/constants/InputPattern.java
@@ -0,0 +1,19 @@
+package christmas.constants;
+
+import java.util.regex.Pattern;
+
+public enum InputPattern {
+    NUMERIC_PATTERN(Pattern.compile("^\\d*$"));
+
+    private final Pattern pattern;
+
+    InputPattern(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public static boolean isNotNumeric(String visitDate) {
+        Pattern pattern = NUMERIC_PATTERN.pattern;
+        return !pattern.matcher(visitDate).matches();
+    }
+}
+
