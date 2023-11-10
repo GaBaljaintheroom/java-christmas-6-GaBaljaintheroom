c39c0b4fad53c5045c0695ce45863d34c0a1bfe8
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 02:06:16 +0900

    feat(InvalidOrderException): 유효하지 않은 주문 형태 예외 객체 생성

---
+++ src/main/java/christmas/exception/InvalidOrderException.java
@@ -0,0 +1,9 @@
+package christmas.exception;
+
+import christmas.constants.ErrorMessage;
+
+public class InvalidOrderException extends IllegalArgumentException {
+    public InvalidOrderException() {
+        super(ErrorMessage.INVALID_ORDER_ERROR.getMessage());
+    }
+}
