c007a35c95394d623f0c244e742252a0258477f2
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 16:34:20 +0900

    feat(Menus): 중복 메뉴 검증 기능 구현

--- src/main/java/christmas/domain/Menus.java
+++ src/main/java/christmas/domain/Menus.java
@@ -1,5 +1,7 @@
 package christmas.domain;
 
+import christmas.exception.InvalidOrderException;
+
 import java.util.List;
 
 public class Menus {
@@ -7,11 +9,29 @@ public class Menus {
     private final List<Menu> values;
 
     private Menus(final List<Menu> values) {
+        validateDuplicateMenu(values);
         this.values = values;
     }
 
     public static Menus from(final List<Menu> values) {
         return new Menus(values);
+    }
+
+    private void validateDuplicateMenu(List<Menu> values) {
+        if (isDuplicatedMenus(values)) {
+            throw new InvalidOrderException();
+        }
+    }
+
+    private boolean isDuplicatedMenus(List<Menu> values) {
+        return values.size() != getMenuCount(values);
+    }
+
+    private long getMenuCount(List<Menu> values) {
+        return values.stream()
+                .map(Menu::getMenuName)
+                .distinct()
+                .count();
     }
 
     public boolean isContainMenuName(final Name name) {
