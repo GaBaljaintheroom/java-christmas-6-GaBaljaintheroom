ea3beef5747672b3d263b4b4b7f46107d3074cc7
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 14:32:20 +0900

    feat(Menu): 메뉴 객체 생성

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,30 @@
+package christmas.domain;
+
+import java.util.Objects;
+
+public class Menu {
+    private final Name menuName;
+    private final Price menuPrice;
+
+    private Menu(final Name name, final Price price) {
+        this.menuName = name;
+        this.menuPrice = price;
+    }
+
+    public static Menu of(final Name name, final Price price) {
+        return new Menu(name, price);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return menuName.equals(menu.menuName) && menuPrice.equals(menu.menuPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menuName, menuPrice);
+    }
+}
