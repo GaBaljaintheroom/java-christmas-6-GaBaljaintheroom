c25d55a7a2c260b39cda84e97b8e5f0f8eab819c
GaBaljaintheroom <junsu1222@naver.com>
2023-11-12 01:26:56 +0900

    feat(TotalOrderPriceFormatter): 할인 전 총 주문 금액 형태 기능 구현

---
+++ src/main/java/christmas/formatter/TotalOrderPriceFormatter.java
@@ -0,0 +1,22 @@
+package christmas.formatter;
+
+import christmas.domain.TotalOrderPrice;
+
+import java.text.NumberFormat;
+
+public final class TotalOrderPriceFormatter {
+
+    private static final String TOTAL_ORDER_PRICE = "<할인 전 총주문 금액>\n";
+    private static final String TOTAL_ORDER_PRICE_FORM = "%s원";
+    private static final StringBuilder sb = new StringBuilder();
+
+    private TotalOrderPriceFormatter() {
+    }
+
+    public static String showTotalOrderPrice(TotalOrderPrice totalOrderPrice) {
+        Integer orderPrice = totalOrderPrice.getOrderPrice();
+        sb.append(TOTAL_ORDER_PRICE)
+                .append(String.format(TOTAL_ORDER_PRICE_FORM, NumberFormat.getInstance().format(orderPrice)));
+        return sb.toString();
+    }
+}
