dd2e9091180bc47dc83d4f4c58b59bea05b190fc
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 14:31:21 +0900

    feat(Name): 메뉴 이름 객체 생성

---
+++ src/main/java/christmas/domain/Name.java
@@ -0,0 +1,29 @@
+package christmas.domain;
+
+import java.util.Objects;
+
+public class Name {
+
+    private final String menuName;
+
+    private Name(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public static Name from(String menuName) {
+        return new Name(menuName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name = (Name) o;
+        return menuName.equals(name.menuName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menuName);
+    }
+}
