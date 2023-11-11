824499a45564230ebfc85ea49e68a50fbc508f48
GaBaljaintheroom <junsu1222@naver.com>
2023-11-12 00:53:47 +0900

    feat(MenuBoardTest): 주문한 메뉴의 총 가격 계산 테스트

--- src/test/java/christmas/constants/MenuBoardTest.java
+++ src/test/java/christmas/constants/MenuBoardTest.java
@@ -1,8 +1,6 @@
 package christmas.constants;
 
-import christmas.domain.Menu;
-import christmas.domain.Name;
-import christmas.domain.Price;
+import christmas.domain.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,8 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 @DisplayName("메뉴판 상수에 대헤")
 class MenuBoardTest {
@@ -25,8 +22,6 @@ class MenuBoardTest {
         assertThatThrownBy(() -> MenuBoard.validateExistMenu(menus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(errorTage);
-
-
     }
 
     static Stream<List<Menu>> notExistMenuSources() {
@@ -44,8 +39,6 @@ class MenuBoardTest {
     void When_ExistMenuInMenuBoard_Then_NotThrowException(List<Menu> menus) {
         assertThatCode(() -> MenuBoard.validateExistMenu(menus))
                 .doesNotThrowAnyException();
-
-
     }
 
     static Stream<List<Menu>> normalMenuSources() {
@@ -54,6 +47,29 @@ class MenuBoardTest {
                 Menu.of(Name.from("타파스"), Price.from(5_500)),
                 Menu.of(Name.from("초코케이크"), Price.from(15_000)),
                 Menu.of(Name.from("레드와인"), Price.from(5_000)))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("normalOrderSources")
+    @DisplayName("주문한 메뉴의 총 가격을 계산한다.")
+    void Given_OrderMenus_When_CalculateTotalOrderPrice_Then_EqualActual(List<Menu> menus) {
+        //given
+        Menus orderMenus = Menus.from(menus);
+
+        //when
+        Integer actual = MenuBoard.calculateTotalOrderPrice(orderMenus);
+
+        //then
+        assertThat(actual).isEqualTo(31_500);
+    }
+
+    static Stream<List<Menu>> normalOrderSources() {
+        return Stream.of(List.of(
+                Menu.of(Name.from("양송이수프"), Amount.from(1)),
+                Menu.of(Name.from("타파스"), Amount.from(1)),
+                Menu.of(Name.from("초코케이크"), Amount.from(1)),
+                Menu.of(Name.from("레드와인"), Amount.from(1)))
         );
     }
 
