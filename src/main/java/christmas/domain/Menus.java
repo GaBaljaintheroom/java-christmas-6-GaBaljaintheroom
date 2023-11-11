d3c5b2e5fff79368a39a8468dceab9fd7d30096d
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 19:02:42 +0900

    feat(Menus): 주문 개수 20개 이하 검증 기능 구현

--- src/main/java/christmas/domain/Menus.java
+++ src/main/java/christmas/domain/Menus.java
@@ -1,15 +1,18 @@
 package christmas.domain;
 
 import christmas.exception.InvalidOrderException;
+import christmas.exception.OverOrderCountException;
 
 import java.util.List;
 
 public class Menus {
 
+    private static final int MAX_ORDER_COUNT = 20;
     private final List<Menu> values;
 
     private Menus(final List<Menu> values) {
         validateDuplicateMenu(values);
+        validateMenuCount(values);
         this.values = values;
     }
 
@@ -33,6 +36,19 @@ public class Menus {
                 .distinct()
                 .count();
     }
+
+    private void validateMenuCount(List<Menu> values) {
+        if (getOrderTotalCount(values) > MAX_ORDER_COUNT) {
+            throw new OverOrderCountException();
+        }
+    }
+
+    private int getOrderTotalCount(List<Menu> values) {
+        return values.stream()
+                .mapToInt(Menu::getMenuAmount)
+                .sum();
+    }
+
 
     public boolean isContainMenuName(final Name name) {
         return values.stream()
