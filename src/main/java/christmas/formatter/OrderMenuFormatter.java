e2bec16b04c2ec095e2b87c515045ae495499072
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 21:42:19 +0900

    feat(OrderMenuFormatter): 주문 메뉴 확인 기능 구현

---
+++ src/main/java/christmas/formatter/OrderMenuFormatter.java
@@ -0,0 +1,25 @@
+package christmas.formatter;
+
+import christmas.domain.Menu;
+import christmas.domain.Menus;
+
+public final class OrderMenuFormatter {
+
+    private static final String ORDER_MENU = "<주문 메뉴>\n";
+    private static final String ORDER_MENU_FORM = "%s %s개\n";
+    private static final StringBuilder sb = new StringBuilder();
+
+    private OrderMenuFormatter() {
+    }
+
+    public static String showOrderMenus(Menus orderMenus) {
+        sb.append(ORDER_MENU);
+        orderMenus.getValues()
+                .forEach(OrderMenuFormatter::getAppend);
+        return sb.toString();
+    }
+
+    private static void getAppend(Menu menu) {
+        sb.append(String.format(ORDER_MENU_FORM, menu.getMenuNameValue(), menu.getMenuAmountValue()));
+    }
+}
