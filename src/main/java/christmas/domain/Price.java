074d21ec6da081f18ece06e5eb3fe1bf09d6ac5c
GaBaljaintheroom <junsu1222@naver.com>
2023-11-11 15:56:24 +0900

    refactor(Price): equals & hashCode 오버라이딩 삭제

--- src/main/java/christmas/domain/Price.java
+++ src/main/java/christmas/domain/Price.java
@@ -1,7 +1,5 @@
 package christmas.domain;
 
-import java.util.Objects;
-
 public class Price {
 
     private final Integer menuPrice;
@@ -12,18 +10,5 @@ public class Price {
 
     public static Price from(Integer menuPrice) {
         return new Price(menuPrice);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Price price = (Price) o;
-        return menuPrice.equals(price.menuPrice);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(menuPrice);
     }
 }
