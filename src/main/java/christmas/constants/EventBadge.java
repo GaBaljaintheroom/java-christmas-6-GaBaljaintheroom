c61cc6d1702a6cc3704bc7b47ffbb0d53c5b8767
GaBaljaintheroom <junsu1222@naver.com>
2023-11-13 01:33:17 +0900

    feat(EventBadge): 이벤트 베지 이벤트 기능 구현

---
+++ src/main/java/christmas/constants/EventBadge.java
@@ -0,0 +1,30 @@
+package christmas.constants;
+
+import christmas.domain.GiveawayMenu;
+import christmas.domain.TotalDiscountPrice;
+
+import java.util.Arrays;
+
+public enum EventBadge {
+
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("별", 5_000),
+    NONE("없음", 0);
+
+    private final String badge;
+    private final Integer totalBenefitPrice;
+    EventBadge(String badge, Integer totalBenefitPrice) {
+        this.badge = badge;
+        this.totalBenefitPrice = totalBenefitPrice;
+    }
+
+    public static String getBadgeByTotalBenefitPrice(TotalDiscountPrice totalDiscountPrice, GiveawayMenu giveawayMenu) {
+        int totalBenefitPrice = totalDiscountPrice.getDiscountPrice() + giveawayMenu.getPrice();
+        return Arrays.stream(values())
+                .filter(eventBadge -> eventBadge.totalBenefitPrice <= totalBenefitPrice)
+                .findFirst()
+                .map(eventBadge -> eventBadge.badge)
+                .orElse(NONE.badge);
+    }
+}
