025a44e896e19d470c34b9ec3f3e7322b9c17251
GaBaljaintheroom <junsu1222@naver.com>
2023-11-13 23:41:37 +0900

    refactor(TotalBenefitPriceFormatter): 총혜택 금액 출력 기능 final 키워드 추가

--- src/main/java/christmas/formatter/TotalBenefitPriceFormatter.java
+++ src/main/java/christmas/formatter/TotalBenefitPriceFormatter.java
@@ -9,29 +9,38 @@ public final class TotalBenefitPriceFormatter {
     private static final String TOTAL_BENEFIT_PRICE_FORM = "-%,d원\n";
     private static final String NONE = "0원\n";
     private static final StringBuilder sb = new StringBuilder();
+    private static final Integer NO_DISCOUNT = 0;
 
     private TotalBenefitPriceFormatter() {
     }
 
-    public static String showExpectedOrderPrice(TotalDiscountPrice totalDiscountPrice,
-                                                GiveawayMenu giveawayMenu) {
+    public static String showExpectedOrderPrice(final TotalDiscountPrice totalDiscountPrice,
+                                                final GiveawayMenu giveawayMenu) {
         sb.append(TOTAL_BENEFIT_PRICE);
-        int totalBenefitPrice = totalDiscountPrice.getDiscountPrice() + giveawayMenu.getPrice();
+        Integer totalBenefitPrice = totalDiscountPrice.getDiscountPrice() + giveawayMenu.getPrice();
         appendTotalBenefitPrice(totalBenefitPrice);
         appendNoTotalBenefitPrice(totalBenefitPrice);
 
         return sb.toString();
     }
 
-    private static void appendTotalBenefitPrice(int totalBenefitPrice) {
-        if (totalBenefitPrice != 0) {
+    private static void appendTotalBenefitPrice(final Integer totalBenefitPrice) {
+        if (isDiscountCase(totalBenefitPrice)) {
             sb.append(String.format(TOTAL_BENEFIT_PRICE_FORM, totalBenefitPrice));
         }
     }
 
-    private static void appendNoTotalBenefitPrice(int totalBenefitPrice) {
-        if (totalBenefitPrice == 0) {
+    private static boolean isDiscountCase(final Integer totalBenefitPrice) {
+        return !totalBenefitPrice.equals(NO_DISCOUNT);
+    }
+
+    private static void appendNoTotalBenefitPrice(final Integer totalBenefitPrice) {
+        if (isNotDiscountCase(totalBenefitPrice)) {
             sb.append(NONE);
         }
+    }
+
+    private static boolean isNotDiscountCase(final Integer totalBenefitPrice) {
+        return totalBenefitPrice.equals(NO_DISCOUNT);
     }
 }
