28cd473a6144a3b476cbba888a47a5447ca63296
GaBaljaintheroom <junsu1222@naver.com>
2023-11-14 23:03:29 +0900

    refactor(DaysEventCategoryTest): MethodSource 이름명 수정

--- src/test/java/christmas/constants/CategoryMenuTest.java
+++ src/test/java/christmas/constants/CategoryMenuTest.java
@@ -1,42 +1,41 @@
 package christmas.constants;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import christmas.constants.message.ErrorMessage;
+import christmas.domain.menu.Amount;
 import christmas.domain.menu.Menu;
 import christmas.domain.menu.Name;
-import christmas.domain.menu.Price;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.List;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @DisplayName("카테고리 메뉴 상수에 대해")
 class CategoryMenuTest {
 
     private static final String errorTage = ErrorMessage.ERROR_TAG.toString();
 
-    static Stream<List<Menu>> onlyBeverageSources() {
+    static Stream<List<Menu>> onlyBeverageOrders() {
         return Stream.of(List.of(
-                Menu.of(Name.from("제로콜라"), Price.from(15_000)),
-                Menu.of(Name.from("레드와인"), Price.from(5_000)),
-                Menu.of(Name.from("샴페인"), Price.from(25_000)))
+                Menu.of(Name.from("제로콜라"), Amount.from(1)),
+                Menu.of(Name.from("레드와인"), Amount.from(1)),
+                Menu.of(Name.from("샴페인"), Amount.from(1)))
         );
     }
 
-    static Stream<List<Menu>> normalMenuSources() {
+    static Stream<List<Menu>> normalMenuOrders() {
         return Stream.of(List.of(
-                Menu.of(Name.from("제로콜라"), Price.from(15_000)),
-                Menu.of(Name.from("레드와인"), Price.from(5_000)),
-                Menu.of(Name.from("바비큐립"), Price.from(25_000)))
+                Menu.of(Name.from("제로콜라"), Amount.from(1)),
+                Menu.of(Name.from("레드와인"), Amount.from(1)),
+                Menu.of(Name.from("바비큐립"), Amount.from(1)))
         );
     }
 
     @ParameterizedTest
-    @MethodSource("onlyBeverageSources")
+    @MethodSource("onlyBeverageOrders")
     @DisplayName("음료만 주문시 예외가 발생한다.")
     void When_OnlyBeverageOrder_Then_ThrowException(List<Menu> menus) {
         assertThatThrownBy(() -> CategoryMenu.validateOnlyBeverageOrder(menus))
@@ -45,7 +44,7 @@ class CategoryMenuTest {
     }
 
     @ParameterizedTest
-    @MethodSource("normalMenuSources")
+    @MethodSource("normalMenuOrders")
     @DisplayName("정상적으로 주문을 한다.")
     void When_InputCorrectOrder_Then_NotThrowException(List<Menu> menus) {
         assertThatCode(() -> CategoryMenu.validateOnlyBeverageOrder(menus))
